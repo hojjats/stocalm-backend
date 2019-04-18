60245ec0e7d71208cf5d7810b611e6f71a17dbbf
tim <timwahlgren@live.se>
2019-04-18 10:50:15 +0200

    add test API and test models

---
+++ src/main/java/com/stocalm/stocalm/Models/Sensor.java
@@ -0,0 +1,37 @@
+package com.stocalm.stocalm.Models;
+
+import java.util.ArrayList;
+
+public class Sensor {
+    private int id;
+    private double x;
+    private double y;
+    private ArrayList<Reading> readings;
+
+    public Sensor(int id, double x, double y) {
+        new Sensor(id, x, y, new ArrayList<>());
+    }
+
+    public Sensor(int id, double x, double y, ArrayList<Reading> readings) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.readings = readings;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public ArrayList<Reading> getReadings() {
+        return readings;
+    }
+
+    public void addReading(Reading reading) {
+        readings.add(reading);
+    }
+}
--- src/main/java/com/stocalm/stocalm/RESTcontroller/sensorController.java
+++ src/main/java/com/stocalm/stocalm/RESTcontroller/sensorController.java
@@ -1,5 +1,7 @@
 package com.stocalm.stocalm.RESTcontroller;
 
+import com.stocalm.stocalm.Models.Reading;
+import com.stocalm.stocalm.Models.Sensor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class sensorController {
 
     @GetMapping("/{sensorId}")
-    public String getSensorById(@PathVariable String sensorId) {
-        return "Test ID: " + sensorId;
+    public Sensor getSensorById(@PathVariable int sensorId) {
+        Sensor s1 = new Sensor(1, 1.3453, 0.53432);
+        s1.addReading(new Reading("2018-01-05", "10:10", 3.2));
+        s1.addReading(new Reading("2018-01-05", "10:20", 3.0));
+        s1.addReading(new Reading("2018-01-05", "10:30", 3.5));
+        Sensor s2 = new Sensor(2, 12.3453, 10.53432);
+        s2.addReading(new Reading("2019-05-01", "20:30", 1.2));
+        s2.addReading(new Reading("2019-05-01", "20:40", 1.1));
+        s2.addReading(new Reading("2019-05-01", "20:50", 1.7));
+
+        switch (sensorId) {
+            case 1:
+                return s1;
+            case 2:
+                return s2;
+            default:
+                return null;
+        }
+
     }
 }
