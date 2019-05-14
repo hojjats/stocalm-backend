614cc243a073a142cc69bb1ab6245f4b9a69dbcc
tim <timwahlgren@live.se>
2019-05-14 16:07:38 +0200

    Add amenities to location model

--- src/main/java/com/stocalm/stocalm/Models/Location.java
+++ src/main/java/com/stocalm/stocalm/Models/Location.java
@@ -1,20 +1,24 @@
 package com.stocalm.stocalm.Models;
 
+import java.util.ArrayList;
+
 public class Location {
     private String name;
     private String imgUrl;
+    private ArrayList<String> amenities;
 
     public Location() {
-
+        this(null);
     }
 
     public Location(String name) {
-        this.name = name;
+        this(name, null, new ArrayList<>());
     }
 
-    public Location(String name, String imgUrl) {
+    public Location(String name, String imgUrl, ArrayList<String> amenities) {
         this.name = name;
         this.imgUrl = imgUrl;
+        this.amenities = amenities;
     }
 
     public String getName() {
@@ -25,12 +29,20 @@ public class Location {
         return imgUrl;
     }
 
+    public ArrayList<String> getAmenities() {
+        return amenities;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public void setAmenities(ArrayList<String> amenities) {
+        this.amenities = amenities;
     }
 
     @Override
